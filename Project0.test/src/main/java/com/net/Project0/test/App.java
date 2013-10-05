a3d6f91f611a59f3c4d5fe9335c01cd7d12e7522
Alliwant63 <wildgs@list.ru>
2013-10-05 22:23:50 +0400

    Revert "Message for others"
    This reverts commit 35c32156303dcfd7e8494c8efb3d9a70a8f5911d.

--- Project0.test/src/main/java/com/net/Project0/test/App.java
+++ Project0.test/src/main/java/com/net/Project0/test/App.java
@@ -6,13 +6,8 @@ package com.net.Project0.test;
  */
 public class App 
 {
-	private static void Hey()
-	{
-		System.out.print("Testing github\n");
-	}
     public static void main( String[] args )
     {
-    	Hey();
         System.out.println( "Hello World!" );
     }
 }
